fb0a1c013414f7852ff1f65c7ffd0ed2fd12b3fb
samuelcolvin <s@muelcolvin.com>
2014-01-16 17:35:42 +0000

    work on c++

--- django/SalesEstimates/admin.py
+++ django/SalesEstimates/admin.py
@@ -84,7 +84,7 @@ class SKUSalesInline(admin.TabularInline):
 
 class CustomerSalesPeriodAdmin(admin.ModelAdmin):
     inlines = [SKUSalesInline]
-    list_display = ('id', 'period', 'customer', 'store_count')
+    list_display = ('id', 'period', 'customer', 'store_count', 'custom_store_count')
     
 admin.site.register(m.CustomerSalesPeriod, CustomerSalesPeriodAdmin)
 
--- worker/test.py
+++ worker/test.py
@@ -3,5 +3,8 @@ connection = 'tcp://127.0.0.1:3306'
 db_name = 'salesestimates'
 user = 'sales-user'
 password = ''
-mysql = worker.MySQL(db_name, user, password, connection)
-mysql.regenerate_csp()
+mysql = worker.MySQL()
+print mysql.connect(db_name, user, password, connection)
+print mysql.clear_csp()
+print mysql.generate_csp()
+
