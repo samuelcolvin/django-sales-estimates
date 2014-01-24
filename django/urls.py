5e15bc849e5cba6f7e01fd7452d1cd438df3b3c5
samuelcolvin <s@muelcolvin.com>
2014-01-24 21:59:07 +0000

    todo changes

--- django/urls.py
+++ django/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 import settings
 import SalesEstimates.views as views
-import ExcelImportExport.forms as imex_forms
+# import ExcelImportExport.forms as imex_forms
 
 from django.contrib import admin
 admin.autodiscover()
@@ -10,6 +10,7 @@ urlpatterns = patterns('',
     url(r'^hot/', include('HotDjango.urls')),
     url(r'^$', views.Index.as_view(), name='index'),
     url(r'^', include('SkeletalDisplay.urls')),
+    url(r'^imex/', include('Imex.urls')),
     url(r'^process$', views.SetupDisplayModel.as_view(), name='process'),
     url(r'^process/(?P<model>\w+)$', views.SetupDisplayModel.as_view(), name='process'),
     url(r'^process/(?P<model>\w+)/(?P<id>\d+)$', views.SetupDisplayItem.as_view(), name='process'),
@@ -19,9 +20,9 @@ urlpatterns = patterns('',
     url(r'^results/(?P<model>\w+)$', views.ResultsDisplayModel.as_view(), name='results'),
     url(r'^results/(?P<model>\w+)/(?P<id>\d+)$', views.ResultsDisplayItem.as_view(), name='results'),
     
-    url(r'^import$', 'ExcelImportExport.forms.upload', name='import'),
-    url(r'^export$', imex_forms.Export.as_view(), name='export'),
-    url(r'^export/(?P<command>\w+)$', imex_forms.Export.as_view(), name='export'),
+#     url(r'^import$', 'ExcelImportExport.forms.upload', name='import'),
+#     url(r'^export$', imex_forms.Export.as_view(), name='export'),
+#     url(r'^export/(?P<command>\w+)$', imex_forms.Export.as_view(), name='export'),
     url(r'^admin/', include(admin.site.urls)),
 )
 
